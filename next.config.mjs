59d7e9cc0c63896a3fa7c1f5c345ecea4b48c367
Naeem Abbas <naeem401080@gmail.com>
2025-05-02 22:26:10 +0500

    Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Add these to ensure proper static generation
-    output: 'standalone', // Or 'export' if you want fully static
-    trailingSlash: true, // Helps with some routing cases
-  }
+   
+    }
   
   export default nextConfig
