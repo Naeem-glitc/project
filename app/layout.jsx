c4105d97cfdeffe56b5b4ca5e3d9051f363b4242
Naeem Abbas <naeem401080@gmail.com>
2025-05-03 18:37:01 +0500

    Commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,18 +5,6 @@ import Footer from "@/Component/Footer";
 import ReduxProvider from './redux/ReduxProvider';
 
 
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "BuyBit ",
   description: "Get the best product here",
@@ -28,7 +16,7 @@ export default function RootLayout({ children }) {
 
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className='antialiased'
       >
          <ReduxProvider>
           {children}
